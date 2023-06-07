65e20adad1e5fe59d6b9ab4e22cc7b6fda7bfe8e
Matthieu <matthieu.tran@utt.fr>
2023-06-07 20:52:11 +0200

    ajout de fonctionnalite comme ajout d'une fonction qui permet de recuperer un utilisateur par son pseudo

---
+++ app/model/ModelPersonne.php
@@ -0,0 +1,254 @@
+<!-- ----- debut ModelPersonne -->
+
+<?php
+require_once 'Model.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+class ModelPersonne
+{
+
+    private $id, $nom, $prenom, $adresse, $login, $password, $statut, $specialite_id;
+
+    public const ADMINISTRATEUR = 0;
+    public const PRATICIEN = 1;
+    public const PATIENT = 2;
+
+    public function __construct($id = NULL, $nom = NULL, $prenom = NULL, $adresse = NULL, $login = NULL, $password = NULL, $statut = NULL, $specialite_id = NULL)
+    {
+        if (!is_null($id)) {
+            $this->id = $id;
+            $this->nom = $nom;
+            $this->prenom = $prenom;
+            $this->adresse = $adresse;
+            $this->login = $login;
+            $this->password = $password;
+            $this->statut = $statut;
+            $this->specialite_id = $specialite_id;
+        }
+    }
+
+    // Setters
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+    }
+
+    public function setAdresse($adresse)
+    {
+        $this->adresse = $adresse;
+    }
+
+    public function setLogin($login)
+    {
+        $this->login = $login;
+    }
+
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    public function setStatut($statut)
+    {
+        $this->statut = $statut;
+    }
+
+    public function setSpecialiteId($specialite_id)
+    {
+        $this->specialite_id = $specialite_id;
+    }
+
+    // Getters
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    public function getAdresse()
+    {
+        return $this->adresse;
+    }
+
+    public function getLogin()
+    {
+        return $this->login;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function getStatut()
+    {
+        return $this->statut;
+    }
+
+    public function getSpecialiteId()
+    {
+        return $this->specialite_id;
+    }
+
+    public static function getStatusLabel($statut)
+    {
+        switch ($statut) {
+            case self::ADMINISTRATEUR:
+                return "Administrateur";
+            case self::PRATICIEN:
+                return "Praticien";
+            case self::PATIENT:
+                return "Patient";
+            default:
+                return "Inconnu";
+        }
+    }
+
+    public static function getAllId()
+    {
+        try {
+            $database = Model::getInstance();
+            $query = "SELECT id FROM personne";
+            $statement = $database->prepare($query);
+            $statement->execute();
+            $results = $statement->fetchAll(PDO::FETCH_COLUMN, 0);
+            return $results;
+        } catch (PDOException $e) {
+            printf("%s - %s<p/>\n", $e->getCode(), $e->getMessage());
+            return NULL;
+        }
+    }
+
+    public static function getMany($query)
+    {
+        try {
+            $database = Model::getInstance();
+            $statement = $database->prepare($query);
+            $statement->execute();
+            $results = $statement->fetchAll(PDO::FETCH_CLASS, "ModelPersonne");
+            return $results;
+        } catch (PDOException $e) {
+            printf("%s - %s<p/>\n", $e->getCode(), $e->getMessage());
+            return NULL;
+        }
+    }
+
+    public static function getAll()
+    {
+        try {
+            $database = Model::getInstance();
+            $query = "SELECT * FROM personne";
+            $statement = $database->prepare($query);
+            $statement->execute();
+            $results = $statement->fetchAll(PDO::FETCH_CLASS, "ModelPersonne");
+            return $results;
+        } catch (PDOException $e) {
+            printf("%s - %s<p/>\n", $e->getCode(), $e->getMessage());
+            return NULL;
+        }
+    }
+
+    public static function getOne($id)
+    {
+        try {
+            $database = Model::getInstance();
+            $query = "SELECT * FROM personne WHERE id = :id";
+            $statement = $database->prepare($query);
+            $statement->execute([
+                'id' => $id
+            ]);
+            $results = $statement->fetchAll(PDO::FETCH_CLASS, "ModelPersonne");
+            return $results;
+        } catch (PDOException $e) {
+            printf("%s - %s<p/>\n", $e->getCode(), $e->getMessage());
+            return NULL;
+        }
+    }
+
+    public static function getUser($login)
+    {
+        try {
+            $database = Model::getInstance();
+            $query = "SELECT * FROM personne WHERE login = :login";
+            $statement = $database->prepare($query);
+            $statement->execute([
+                'login' => $login
+            ]);
+            $results = $statement->fetchAll(PDO::FETCH_CLASS, "ModelPersonne");
+            return $results;
+        } catch (PDOException $e) {
+            printf("%s - %s<p/>\n", $e->getCode(), $e->getMessage());
+            return NULL;
+        }
+    }
+
+    public static function insert($nom, $prenom, $adresse, $login, $password, $statut, $specialite_id)
+    {
+        try {
+            $database = Model::getInstance();
+
+            // Recherche de la valeur de la clÃ© = max(id) + 1
+            $query = "SELECT MAX(id) FROM personne";
+            $statement = $database->query($query);
+            $tuple = $statement->fetch();
+            $id = $tuple['0'];
+            $id++;
+
+            // Ajout d'un nouveau tuple
+            $query = "INSERT INTO personne VALUE (:id, :nom, :prenom, :adresse, :login, :password, :statut, :specialite_id)";
+            $statement = $database->prepare($query);
+            $statement->execute([
+                'id' => $id,
+                'nom' => $nom,
+                'prenom' => $prenom,
+                'adresse' => $adresse,
+                'login' => $login,
+                'password' => $password,
+                'statut' => $statut,
+                'specialite_id' => $specialite_id
+            ]);
+            return $id;
+        } catch (PDOException $e) {
+            printf("%s - %s<p/>\n", $e->getCode(), $e->getMessage());
+            return -1;
+        }
+    }
+
+    public static function delete($id)
+    {
+        try {
+            $database = Model::getInstance();
+            $query = "DELETE FROM personne WHERE id=:id";
+            $statement = $database->prepare($query);
+            $statement->execute([
+                'id' => $id
+            ]);
+            return true;
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+}
+?>
+<!-- ----- fin ModelPersonne -->
