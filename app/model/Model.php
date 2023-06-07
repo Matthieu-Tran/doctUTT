b222a7fc36e796f7ba99667f38852b6c081acf63
Matthieu <matthieu.tran@utt.fr>
2023-06-07 20:53:41 +0200

    petits changement dans le model

--- app/model/Model.php
+++ app/model/Model.php
@@ -1,34 +1,34 @@
-
 <!-- ----- debut Model -->
 <?php
 
-class Model extends PDO {
+class Model extends PDO
+{
 
- private static $_instance;
+    private static $_instance;
 
- // Constructeur : héritage public obligatoire par héritage de PDO
- public function __construct() {
- }
+    // Constructeur : héritage public obligatoire par héritage de PDO
+    public function __construct()
+    {
+    }
 
- //Singleton
- public static function getInstance() {
-  // les variables sont définies dans le fichier config.php
-  include_once '../controller/config.php';
-  
-  if (DEBUG) echo ("Model : getInstance : dsn = $dsn</br>");
+    //Singleton
+    public static function getInstance()
+    {
+        // les variables sont définies dans le fichier config.php
+        include_once '../controller/config.php';
+        if (DEBUG) echo ("Model : getInstance : dsn = $dsn</br>");
 
-  $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+        $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 
-  if (!isset(self::$_instance)) {
-   try {
-    self::$_instance = new PDO($dsn, $username, $password, $options);
-   } catch (PDOException $e) {
-    printf("%s - %s<p/>\n", $e->getCode(), $e->getMessage());
-   }
-  }
-  return self::$_instance;
- }
-
+        if (!isset(self::$_instance)) {
+            try {
+                self::$_instance = new PDO($dsn, $username, $password, $options);
+            } catch (PDOException $e) {
+                printf("%s - %s<p/>\n", $e->getCode(), $e->getMessage());
+            }
+        }
+        return self::$_instance;
+    }
 }
 ?>
 <!-- ----- fin Model -->
