d44c3d9dbb0b8121fc0257bf99b133d9e87fd418
Matthieu <matthieu.tran@utt.fr>
2023-06-08 20:46:24 +0200

    ajout des vues pour afficher toutes les specialites et celui ou on choisit qu'un seul

---
+++ app/view/administrateur/viewSelectionId.php
@@ -0,0 +1,21 @@
+<body>
+    <div class="container p-5">
+        <h1>Choisissez l'ID de la spécialité</h1>
+
+        <form role="form" method='get' action='router.php'>
+            <div class="form-group">
+                <input type="hidden" name='action' value='<?php echo ($target); ?>'>
+                <label for="id">id : </label> <select class="form-control" id='id' name='id' style="width: 100px">
+                    <?php
+                    foreach ($results as $id) {
+                        echo ("<option>$id</option>");
+                    }
+                    ?>
+                </select>
+            </div>
+            <p /><br />
+            <button class="btn btn-primary" type="submit">Submit form</button>
+        </form>
+        <p />
+    </div>
+</body>
