b67a5d42fc21c0dc637850b1eb53f78f02a058cd
Matthieu <matthieu.tran@utt.fr>
2023-06-14 09:26:41 +0200

    merge

--- app/view/praticien/viewInsertRdv.php
+++ app/view/praticien/viewInsertRdv.php
@@ -6,25 +6,23 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 ?>
 
+<div class="container rounded" style="max-width: 500px;">
+    <div class="card-body">
+        <form role="form" method="POST" action="router.php?action=rdvCreated">
+            <div class="form-group">
+                <input type="hidden" name='action' value='rdvCreated'>
+                <label for="date">Date :</label>
+                <input type="date" id="date" name="date" min="<?php echo date('Y-m-d'); ?>" size='75' required><br>
 
-<form role="form" method="POST" action="router.php?action=rdvCreated">
-    <div class="form-group">
-        <input type="hidden" name='action' value='rdvCreated'>
-        <label for="date">Date :</label>
-        <input type="date" id="date" name="date" min="<?php echo date('Y-m-d'); ?>" size='75' required><br>
-
-        <label for="nombre_rdv">Nombre de rendez-vous :</label>
-        <input type="number" id="nombre_rdv" name="nombre_rdv" min="1" max="8" size='75' required><br>
+                <label for="nombre_rdv">Nombre de rendez-vous :</label>
+                <input type="number" id="nombre_rdv" name="nombre_rdv" min="1" max="8" size='75' required><br>
+            </div>
+            <p />
+            <button class="btn btn-primary" type="submit">Go</button>
+        </form>
     </div>
-    <p />
-    <button class="btn btn-primary" type="submit">Go</button>
-</form>
-
-<p />
-<button class="btn btn-primary" type="submit">Go</button>
-</form>
-<p />
 </div>
+
 
 
 <!-- ----- fin viewInsert -->
